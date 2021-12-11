489aae5b1cb16cd15e88974b1473471468b092c1
Your Name <youremail@yourdomain.com>
2021-12-11 15:03:11 +0100

    webprog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BootstrapComponent } from './bootstrap/bootstrap.component';
+import { CssComponent } from './css/css.component';
+import { HtmlComponent } from './html/html.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HtmlComponent},
+  {path: "/css", component: CssComponent},
+  {path: "/bootstrap", component: BootstrapComponent},
+  {path: "/javascript", component: JavascriptComponent},
+  {path: "**", component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
+import { BootstrapComponent } from './bootstrap/bootstrap.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HtmlComponent,
+    CssComponent,
+    BootstrapComponent,
+    JavascriptComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
